ff990210cbe4f2169360a33da8dae63cbbaeb9bc
Rinyaresu <kaiquelinhares17@outlook.com>
2022-05-03 19:20:49 -0300

    rubocop -A

--- app/controllers/market_lists_controller.rb
+++ app/controllers/market_lists_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class MarketListsController < ApplicationController
   def index; end
 end
--- test/controllers/market_lists_controller_test.rb
+++ test/controllers/market_lists_controller_test.rb
@@ -1,27 +1,29 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class MarketListsControllerTest < ActionDispatch::IntegrationTest
-  test 'Should show your market list as title if access root' do
+  test "Should show your market list as title if access root" do
     get root_path
     assert_response :success
-    assert_select 'h1', text: 'Suas listas de mercado'
+    assert_select "h1", text: "Suas listas de mercado"
   end
 
-  test 'Should show your market list as title if access specific url' do
+  test "Should show your market list as title if access specific url" do
     get market_lists_path
     assert_response :success
-    assert_select 'h1', text: 'Suas listas de mercado'
+    assert_select "h1", text: "Suas listas de mercado"
   end
 
-  test 'Should show you dont have any market list if you dont have any list if access root' do
+  test "Should show you dont have any market list if you dont have any list if access root" do
     get root_path
     assert_response :success
-    assert_select 'p', text: 'Você ainda não possui nenhuma lista.'
+    assert_select "p", text: "Você ainda não possui nenhuma lista."
   end
 
-  test 'Should show you dont have any list if you dont have any list if access specific url' do
+  test "Should show you dont have any list if you dont have any list if access specific url" do
     get market_lists_path
     assert_response :success
-    assert_select 'p', text: 'Você ainda não possui nenhuma lista.'
+    assert_select "p", text: "Você ainda não possui nenhuma lista."
   end
 end
